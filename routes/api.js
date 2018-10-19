625e296af84af324956fc5b81a082519da092060
89s52 <25033402+89s52@users.noreply.github.com>
2018-10-19 15:28:06 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(){
+  responseData = {
+     code:0,
+     message:''
+  }
+});
+
+
 /* GET users listing. */
 router.post('/user/register', function(req, res, next) {
   console.log(req.body);
