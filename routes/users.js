b3de27c81d57dcdb3eb7784ec16abfff713e9047
89s52 <8888v@163.com>
2018-10-21 20:02:44 +0800

    10

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var User = require('../models/User');
+var User = require('../models/user.server.model');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
