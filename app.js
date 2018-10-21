a0f22596c6b61c2416a278afb4bf848ff235fb40
89s52 <8888v@163.com>
2018-10-21 20:15:59 +0800

    11

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({extended:false}));
 
 app.use('/', indexRouter);
 app.use('/user', user);
