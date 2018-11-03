d27f4bbf2a68c20754c352a2bfd1f3a4780d5650
89s52 <8888v@163.com>
2018-11-03 10:14:35 +0800

    1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 ﻿var createError = require('http-errors');
 var express = require('express');
-var mongoose = require('mongoose');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
---
+++ schemas/股票行情JSON.js
@@ -0,0 +1,79 @@
+{
+  "code": 0,
+  "data": {
+    "securityCode": "SH.600011",
+    "name": "华能国际",
+    "now": 6580,
+    "close": 6560,
+    "open": 6540,
+    "high": 6600,
+    "low": 6530,
+    "volume": 910200,
+    "bid1": 6560,
+    "bid2": 6550,
+    "bid3": 6540,
+    "bid4": 6530,
+    "bid5": 6520,
+    "bid1Volume": 100,
+    "bid2Volume": 13300,
+    "bid3Volume": 34900,
+    "bid4Volume": 73600,
+    "bid5Volume": 64500,
+    "ask1": 6580,
+    "ask2": 6590,
+    "ask3": 6600,
+    "ask4": 6610,
+    "ask5": 6620,
+    "ask1Volume": 141000,
+    "ask2Volume": 136200,
+    "ask3Volume": 179500,
+    "ask4Volume": 169700,
+    "ask5Volume": 174400,
+    "transactions": [
+      {
+        "time": "09:37:43",
+        "price": 6580,
+        "volume": 100,
+        "action": "B",
+        "amount": 65800000
+      },
+      {
+        "time": "09:37:40",
+        "price": 6550,
+        "volume": 10,
+        "action": "S",
+        "amount": 6550000
+      },
+      {
+        "time": "09:37:28",
+        "price": 6550,
+        "volume": 47,
+        "action": "S",
+        "amount": 30801000
+      },
+      {
+        "time": "09:37:22",
+        "price": 6560,
+        "volume": 150,
+        "action": "S",
+        "amount": 98400000
+      },
+      {
+        "time": "09:37:19",
+        "price": 6580,
+        "volume": 100,
+        "action": "B",
+        "amount": 65800000
+      },
+      {
+        "time": "09:37:13",
+        "price": 6580,
+        "volume": 5,
+        "action": "B",
+        "amount": 3290000
+      }
+    ],
+    "datetime": "2017-12-18 09:37:43"
+  },
+  "msg": "success"
+}
