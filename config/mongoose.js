b48b53564028629b133ba810d690b3e277043b40
89s52 <8888v@163.com>
2018-11-05 19:51:34 +0800

    2

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,10 +2,11 @@ var mongoose = require('mongoose');
 var config = require('./config.js');
 
 module.exports = function(){
-  var db = mongoose.connect(config.mongodb);
+  var db = mongoose.connect(config.mongodb,{ useNewUrlParser: true });
 
   require('../models/user.server.model.js');
   require("../models/admin.server.model.js");
- 
+  require("../models/stock.market.model.js");
+
   return db;
 };
